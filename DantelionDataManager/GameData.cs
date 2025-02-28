2dfe037d90d87c78f6e75b5fc5a0f1c547f9dcfd
kotn3l <kotn3l@gmail.com>
2025-03-01 00:58:16 +0100

    Separate GameFile into its own .cs

--- DantelionDataManager/GameData.cs
+++ DantelionDataManager/GameData.cs
@@ -9,34 +9,6 @@ using System.Text.RegularExpressions;
 
 namespace DantelionDataManager
 {
-    public class GameFile
-    {
-        public string Path { get; private set; }
-        public Memory<byte> Bytes { get; private set; }
-        public ISoulsFile Data { get; set; }
-
-        public GameFile(string path, Memory<byte> bytes)
-        {
-            Path = path;
-            Bytes = bytes;
-        }
-
-        public GameFile(string path, Memory<byte> bytes, Func<Memory<byte>, ISoulsFile> load) : this(path, bytes)
-        {
-            Load(load);
-        }
-
-        public void Load(Func<Memory<byte>, ISoulsFile> load)
-        {
-            Data = load(Bytes);
-        }
-
-        public void Write(GameData data)
-        {
-            Bytes = Data?.Write();
-            data.SetMem(Path, Bytes);
-        }
-    }
     public abstract class GameData
     {
         protected static readonly HashSet<string>_nonCompressedExceptions = ["hks", "bdt", "bhd", "bin", "plt", "prx", "dat", "sha", "dds", "png", "sfo", "xml", "sig", "info", "sprx" ];
---
+++ DantelionDataManager/GameFile.cs
@@ -0,0 +1,33 @@
+﻿using SoulsFormats;
+
+namespace DantelionDataManager
+{
+    public class GameFile
+    {
+        public string Path { get; private set; }
+        public Memory<byte> Bytes { get; private set; }
+        public ISoulsFile Data { get; set; }
+
+        public GameFile(string path, Memory<byte> bytes)
+        {
+            Path = path;
+            Bytes = bytes;
+        }
+
+        public GameFile(string path, Memory<byte> bytes, Func<Memory<byte>, ISoulsFile> load) : this(path, bytes)
+        {
+            Load(load);
+        }
+
+        public void Load(Func<Memory<byte>, ISoulsFile> load)
+        {
+            Data = load(Bytes);
+        }
+
+        public void Write(GameData data)
+        {
+            Bytes = Data?.Write();
+            data.SetMem(Path, Bytes);
+        }
+    }
+}
